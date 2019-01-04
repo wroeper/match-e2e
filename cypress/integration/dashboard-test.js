551c0640eda3c6a8e88bc1ed9d6d2f9f15298c0c
Wilfried Roeper <mail@wroeper.de>
2019-01-04 21:35:22 +0100

    initial commit

---
+++ cypress/integration/dashboard-test.js
@@ -0,0 +1,6 @@
+describe('Match Dashboard Test', function() {
+  it('visits the match dashboard', function() {
+    cy.visit('http://wr-lb-match-dash-int-394575891.eu-central-1.elb.amazonaws.com/')
+  	cy.contains("funnny games")
+  })
+})
